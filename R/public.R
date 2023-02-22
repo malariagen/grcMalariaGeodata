07dad020684809589446f766279b7a5af926078d
Olivo Miotto <Olivo@tropmedres.ac>
2023-02-23 00:47:50 +0700

    Added accessor function.

---
+++ R/public.R
@@ -0,0 +1,14 @@
+#############################################################
+#
+#' Load geographical data for Genetic Report Cards map plotting using the grcMAlaria package
+#'
+#'
+#' @return A list containing ...
+#' @export
+#'
+#' @examples
+#' #TBD
+#
+getGeoTables <- function () {
+    map.geoTables
+}
